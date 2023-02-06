4a74251411625bbfbf7dbb2a8f311d808bc58220
amllan123 <99805797+amllan123@users.noreply.github.com>
2023-02-07 00:16:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 5000,(req,res)=>{
+    console.log("Server Connected");
+})
