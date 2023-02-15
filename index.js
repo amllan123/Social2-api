1ce2099e0869172fda21d7ecf9f3a0ee4183025f
Amllan Bhukta <99805797+amllan123@users.noreply.github.com>
2023-02-16 00:41:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const server= app.listen(process.env.PORT || 5000,(req,res)=>{
 const socket=require("socket.io")
 const io = socket(server, {
    cors: {
-     origin: "http://localhost:3000",
+     origin: "https://social2-frontend.vercel.app",
      credentials: true,
    },
  });
